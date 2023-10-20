a14606c75b40803bc8c0241ab7bfc665311e22f0
Natitatalia <nati.mius@gmail.com>
2023-10-20 17:32:16 -0500

    cambios js

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ let titulo = document.querySelector("h1");
 titulo.textContent="carajito"
 
 
-let tituloh3 = document.querySelector("h3");
+let tituloh3 = document.querySelector("h2");
 tituloh3.onClick() = () => {
 	alert( "auch no mas" );
 };
