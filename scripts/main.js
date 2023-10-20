303709e1fdf5068d753dc80167d393f3faae25c2
Natitatalia <nati.mius@gmail.com>
2023-10-20 17:35:47 -0500

    correcion js

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,6 @@ let titulo = document.querySelector("h1");
 titulo.textContent="carajito"
 
 
-let tituloh3 = document.querySelector("h2");
-tituloh3.onclick() = () => {
+document.querySelector("h2").onclick() = () => {
 	alert( "auch no mas" );
 };
